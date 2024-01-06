0c8f1ef826b81ef4e8dd6d1a427dbb2afe2d06cb
Renato Lacerda <renato.ac.lacerda@gmail.com>
2024-01-06 18:37:17 -0300

    feat: creating new "BaseCard" component

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card">
+    <h2><slot name="title" /></h2>
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 2rem;
+  border-radius: 1rem;
+  background-color: var(--color-background-soft);
+  border: 1px solid var(--color-itens);
+  width: 300px;
+  text-align: center;
+}
+</style>
